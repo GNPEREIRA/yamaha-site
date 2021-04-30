af4a81d31fe85942181b6402cd16459a55f805e4
GNPEREIRA <guspereira2010@gmail.com>
2021-04-30 16:15:10 -0300

    Inicial

---
+++ js/owl/setup.js
@@ -0,0 +1,52 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+
+$('.owl-carousel-auto-height').owlCarousel({
+    items:1,
+    margin:10,
+    autoHeight:true
+});
+
+{
+    AutoHeight.Defaults = {
+        autoHeight: false,
+        autoHeightClass: 'owl-height'
+    };
+}
+
+$('.loop').owlCarousel({
+    center: true,
+    items:2,
+    loop:true,
+    margin:10,
+    responsive:{
+        600:{
+            items:4
+        }
+    }
+});
+$('.nonloop').owlCarousel({
+    center: true,
+    items:2,
+    loop:false,
+    margin:10,
+    responsive:{
+        600:{
+            items:4
+        }
+    }
+});
